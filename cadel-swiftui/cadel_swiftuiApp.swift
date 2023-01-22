5f21b0e075cb7fcb59e65f74ba483f25dd1badf5
hafizhMo <hafizh.hizb@gmail.com>
2023-01-23 05:53:07 +0700

    Initial Commit

---
+++ cadel-swiftui/cadel_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cadel_swiftuiApp.swift
+//  cadel-swiftui
+//
+//  Created by Hafizh Mo on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct cadel_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
